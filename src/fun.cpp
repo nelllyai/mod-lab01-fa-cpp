fcba7df347a6911059c4637ac7b27348230ca3c3
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-03-08 16:49:04 +0300

    Добавлена функция faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,24 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool inWordWithoutNumbers = false; //флаг ("находимся внутри слова без цифр")
+	unsigned int result = 0; //количество слов без цифр в строке
+	int i;
+
+	for (i = 0; i < strlen(str); i++) {
+		if ((str[i] == ' ' || i == strlen(str) - 1) && inWordWithoutNumbers) { //вышли из очередного слова без цифры
+			result++;
+			inWordWithoutNumbers = false;
+		}
+		else if (!inWordWithoutNumbers && str[i] != ' ' && !isdigit(str[i]) && (i == 0 || str[i - 1] == ' ')) { //вошли в слово
+			inWordWithoutNumbers = true;
+		}
+		else if (inWordWithoutNumbers && isdigit(str[i])) { //в слове наткнулись на цифру
+			inWordWithoutNumbers = false;
+		}
+	}
+
+	return result;
 }
 
 unsigned int faStr2(const char *str) {
